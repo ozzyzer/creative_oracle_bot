1372fe09df661b1387828b3c3c6b2a777636d47f
Egor Zernov <93536172+ozzyzer@users.noreply.github.com>
2022-11-14 16:01:42 +0300

    Update main.py
    got rid off repeated code, added definition inline button

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import telebot
 from telebot import types
 import os
 
-bot = telebot.TeleBot('TOKENBOT')
-taro = os.listdir('path to jpg files')
+def getting_taro(card_number, user_message):
+    new_taro = set()
+    while len(new_taro) != card_number:
+        new_elem = choice(taro)
+        new_taro.add(choice(taro))
+    card_number = 0
+    for i_elem in new_taro:
+        card_number += 1
+        bot.send_photo(user_message.chat.id, photo=open(f"path\{i_elem}", "rb"), caption=f"{str(card_number)} карта")
+    markup = types.InlineKeyboardMarkup()
+    link = types.InlineKeyboardButton("Толкование карт", url='https://magya-online.ru/znachenie_kart_taro_raydera_weyta')
+    markup.add(link)
+    bot.send_message(user_message.chat.id, "Перейди на сайт, чтобы посмотреть толкование карт\nРасклады только на старших арканах", reply_markup=markup)
+
+bot = telebot.TeleBot('token')
+taro = os.listdir('path\\taro')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -22,6 +36,7 @@ def func(message):
 
     elif message.text == "Таро":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup_1 = types.InlineKeyboardMarkup()
         btn1 = types.KeyboardButton("Три карты")
         btn2 = types.KeyboardButton("Крест")
         btn3 = types.KeyboardButton("Пирамида любви")
@@ -31,39 +46,16 @@ def func(message):
 
     elif message.text == "Три карты":
         bot.send_message(message.chat.id, text="Ваше прошлое (карта 1), ваше настоящее (карта 2) и ваше будущее (карта 3)")
-        new_taro = set()
-        while len(new_taro) != 3:
-            new_elem = choice(taro)
-            new_taro.add(choice(taro))
-        card_number = 0
-        for i_elem in new_taro:
-            card_number += 1
-            bot.send_photo(message.chat.id, photo=open(f"path to jpg/{i_elem}", "rb"), caption=f"{str(card_number)} карта")
+        getting_taro(3, message)
 
     elif message.text == "Крест":
         bot.send_message(message.chat.id, text="Смысл проблемы (карта 1), чего не следует делать. (карта 2),что следует сделать (карта 3) и что получится, к чему это приведет (4 карта)")
-        new_taro = set()
-        while len(new_taro) != 4:
-            new_elem = choice(taro)
-            new_taro.add(choice(taro))
-        card_number = 0
-        for i_elem in new_taro:
-            card_number += 1
-            bot.send_photo(message.chat.id, photo=open(f"path to jpg/{i_elem}", "rb"), caption=f"{str(card_number)} карта")
-
+        getting_taro(4, message)
 
     elif message.text == "Пирамида любви":
         bot.send_message(message.chat.id,
                          text="Первая позиция - ВЫ: эта карта показывает в каком состоянии находитесь вы в текущей ситуации. То есть только то, что касается вашего внутреннего состояния относительно отношений с партнером \nВторая позиция - ПАРТНЕР: эта карта помогает понять вашего партнера и разобраться как его поведение влияет на вас, как он относятся к вам.\nТретья позиция - ОТНОШЕНИЯ: эта карта говорит о ваших отношениях, которые складываются между вами. И дает возможность вам понять, что происходит между вами и какие стоит сделать выводы.\nЧетвертая позиция - БУДУЩЕЕ: эта карта дает предпосылки для того, чтобы понять какое может быть будущее у вас исходя из вашего текущего поведения, текущего поведения вашего партнера и тех отношений, которые сложились у вас")
-        new_taro = set()
-        while len(new_taro) != 4:
-            new_elem = choice(taro)
-            new_taro.add(choice(taro))
-        card_number = 0
-        for i_elem in new_taro:
-            card_number += 1
-            bot.send_photo(message.chat.id, photo=open(f"path to jpg/{i_elem}", "rb"), caption=f"{str(card_number)} карта")
-
+        getting_taro(4, message)
 
     elif message.text == "Вернуться в главное меню":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
